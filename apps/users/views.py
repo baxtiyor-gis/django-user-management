cd396b87b4c7b003e82b1db5122f9cab2a904d63
Baxtiyor <01baxtiyor@gmail.com>
2025-11-20 23:15:58 +0500

    Add user registration functionality with custom form and template

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from .forms import CustomRegisterForms
+
+
 def login_page(request):
     if request.user.is_authenticated:
         return redirect('home_page')
@@ -20,6 +23,21 @@ def login_page(request):
             messages.error(request, "Login yoki parol xato!")
             return redirect('login_page')
     return render(request, template_name='users/login.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = CustomRegisterForms(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # avtomatik login
+            messages.success(request, "Muvaffaqiyatli ro‘yxatdan o‘tdingiz!")
+            return redirect('profile')
+        else:
+            messages.error(request, "Ma'lumotlarda xatolik bor. Tekshirib qayta urinib ko‘ring.")
+
+    else:
+        form = CustomRegisterForms()
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def logout_page(request):
